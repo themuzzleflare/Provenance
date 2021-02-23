42ee26b2eb6b69bbc0d9e779ebe3bd5fc7fd54f8
Paul Tavitian <paul.tavitian@icloud.com>
2021-02-23 15:40:48 +1100

    5.1

--- Podfile
+++ Podfile
@@ -4,10 +4,6 @@ target 'Provenance' do
 
   use_frameworks!
   
-  pod 'Firebase/Analytics'
-  pod 'Firebase/Messaging'
-  pod 'Firebase/Crashlytics'
-  pod 'Firebase/Performance'
   pod 'MarqueeLabel'
   pod 'NotificationBannerSwift', '~> 3.0.0'
   
--- Provenance/AppDelegate.swift
+++ Provenance/AppDelegate.swift
@@ -1,9 +1,10 @@
 import UIKit
-import Firebase
+#if !targetEnvironment(macCatalyst)
 import NotificationBannerSwift
+#endif
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
@@ -16,25 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         window?.rootViewController = viewController
         
         window?.makeKeyAndVisible()
-        
-        FirebaseApp.configure()
-        
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
-        
-        application.registerForRemoteNotifications()
-        
-        Messaging.messaging().delegate = self
         
         if UserDefaults.standard.string(forKey: "apiKey") == "" || UserDefaults.standard.string(forKey: "apiKey") == nil {
             let ac = UIAlertController(title: "API Key Required", message: "You don't have an API Key set. Set one now.", preferredStyle: .alert)
@@ -60,33 +42,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                             if statusCode == 200 {
                                 DispatchQueue.main.async {
                                     UserDefaults.standard.set(answer.text!, forKey: "apiKey")
+                                    #if !targetEnvironment(macCatalyst)
                                     let banner = NotificationBanner(title: "Success", subtitle: "The API Key was verified and set.", leftView: nil, rightView: nil, style: .success, colors: nil)
                                     banner.duration = 2
                                     settingsController.appearingBanner = banner
+                                    #endif
                                     self.window?.rootViewController?.present(UINavigationController(rootViewController: settingsController), animated: true)
                                 }
                             } else {
                                 DispatchQueue.main.async {
+                                    #if !targetEnvironment(macCatalyst)
                                     let banner = NotificationBanner(title: "Failed", subtitle: "The API Key could not be verified.", leftView: nil, rightView: nil, style: .danger, colors: nil)
                                     banner.duration = 2
                                     settingsController.appearingBanner = banner
+                                    #endif
                                     self.window?.rootViewController?.present(UINavigationController(rootViewController: settingsController), animated: true)
                                 }
                             }
                         } else {
                             DispatchQueue.main.async {
+                                #if !targetEnvironment(macCatalyst)
                                 let banner = NotificationBanner(title: "Failed", subtitle: error?.localizedDescription ?? "The API Key could not be verified.", leftView: nil, rightView: nil, style: .danger, colors: nil)
                                 banner.duration = 2
                                 settingsController.appearingBanner = banner
+                                #endif
                                 self.window?.rootViewController?.present(UINavigationController(rootViewController: settingsController), animated: true)
                             }
                         }
                     }
                     .resume()
                 } else {
+                    #if !targetEnvironment(macCatalyst)
                     let banner = NotificationBanner(title: "Failed", subtitle: "The provided API Key was either empty, or the same as the current one.", leftView: nil, rightView: nil, style: .danger, colors: nil)
                     banner.duration = 2
                     settingsController.appearingBanner = banner
+                    #endif
                     self.window?.rootViewController?.present(UINavigationController(rootViewController: settingsController), animated: true)
                 }
             }
@@ -98,10 +88,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
         
         return true
-    }
-    
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        let dataDict:[String: String] = ["token": fcmToken ?? ""]
-        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
     }
 }
