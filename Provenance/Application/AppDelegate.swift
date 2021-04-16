070cc59fc0e8373b3c40c3b00cf3002dd60b15a0
Paul Tavitian <paul.tavitian@icloud.com>
2021-04-16 19:36:51 +1000

    11.0

--- Provenance/Application/AppDelegate.swift
+++ Provenance/Application/AppDelegate.swift
@@ -5,7 +5,8 @@ import Rswift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey: Any]?) -> Bool {        registerDefaultsFromSettingsBundle()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        registerDefaultsFromSettingsBundle()
         
         FirebaseApp.configure()
         Analytics.load()
