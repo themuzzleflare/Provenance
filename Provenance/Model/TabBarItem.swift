cdfae6cb885bc7195c3553f6fe70896ef78db6b0
Paul Tavitian <paul.tavitian@icloud.com>
2021-06-11 13:22:08 +1000

    15.9

---
+++ Provenance/Model/TabBarItem.swift
@@ -0,0 +1,66 @@
+import UIKit
+import Rswift
+
+enum TabBarItem: Int, CaseIterable {
+    case transactions, accounts, tags, categories, about
+
+    func vc() -> UIViewController {
+        switch self {
+            case .transactions:
+                return NavigationController(rootViewController: TransactionsVC(style: .grouped))
+            case .accounts:
+                return NavigationController(rootViewController: AccountsCVC(collectionViewLayout: twoColumnGridLayout()))
+            case .tags:
+                return NavigationController(rootViewController: AllTagsVC(style: .grouped))
+            case .categories:
+                return NavigationController(rootViewController: CategoriesCVC(collectionViewLayout: twoColumnGridLayout()))
+            case .about:
+                return NavigationController(rootViewController: AboutVC(style: .grouped))
+        }
+    }
+
+    func title() -> String {
+        switch self {
+            case .transactions:
+                return "Transactions"
+            case .accounts:
+                return "Accounts"
+            case .tags:
+                return "Tags"
+            case .categories:
+                return "Categories"
+            case .about:
+                return "About"
+        }
+    }
+    
+    func image() -> UIImage? {
+        switch self {
+            case .transactions:
+                return R.image.dollarsignCircle()
+            case .accounts:
+                return R.image.walletPass()
+            case .tags:
+                return R.image.tag()
+            case .categories:
+                return R.image.arrowUpArrowDownCircle()
+            case .about:
+                return R.image.infoCircle()
+        }
+    }
+
+    func selectedImage() -> UIImage? {
+        switch self {
+            case .transactions:
+                return R.image.dollarsignCircleFill()
+            case .accounts:
+                return R.image.walletPassFill()
+            case .tags:
+                return R.image.tagFill()
+            case .categories:
+                return R.image.arrowUpArrowDownCircleFill()
+            case .about:
+                return R.image.infoCircleFill()
+        }
+    }
+}
--- Provenance/View Controllers/TabBarController.swift
+++ Provenance/View Controllers/TabBarController.swift
@@ -18,32 +18,10 @@ class TabBarController: UITabBarController {
 
 private extension TabBarController {
     private func configure() {
-        viewControllers = [
-            {
-                let vc = NavigationController(rootViewController: TransactionsVC(style: .grouped))
-                vc.tabBarItem = UITabBarItem(title: "Transactions", image: R.image.dollarsignCircle(), selectedImage: R.image.dollarsignCircleFill())
-                return vc
-            }(),
-            {
-                let vc = NavigationController(rootViewController: AccountsCVC(collectionViewLayout: twoColumnGridLayout()))
-                vc.tabBarItem = UITabBarItem(title: "Accounts", image: R.image.walletPass(), selectedImage: R.image.walletPassFill())
-                return vc
-            }(),
-            {
-                let vc = NavigationController(rootViewController: AllTagsVC(style: .grouped))
-                vc.tabBarItem = UITabBarItem(title: "Tags", image: R.image.tag(), selectedImage: R.image.tagFill())
-                return vc
-            }(),
-            {
-                let vc = NavigationController(rootViewController: CategoriesCVC(collectionViewLayout: twoColumnGridLayout()))
-                vc.tabBarItem = UITabBarItem(title: "Categories", image: R.image.arrowUpArrowDownCircle(), selectedImage: R.image.arrowUpArrowDownCircleFill())
-                return vc
-            }(),
-            {
-                let vc = NavigationController(rootViewController: AboutVC(style: .grouped))
-                vc.tabBarItem = UITabBarItem(title: "About", image: R.image.infoCircle(), selectedImage: R.image.infoCircleFill())
-                return vc
-            }()
-        ]
+        viewControllers = TabBarItem.allCases.map { item in
+            let vc = item.vc()
+            vc.tabBarItem = UITabBarItem(title: item.title(), image: item.image(), selectedImage: item.selectedImage())
+            return vc
+        }
     }
 }
