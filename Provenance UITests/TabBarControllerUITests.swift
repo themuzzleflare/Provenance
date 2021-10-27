29133f5a46f085f173e8b9a5ed7589046f656ddc
Paul Tavitian <paul.tavitian@icloud.com>
2021-10-27 18:04:41 +1100

    24.0.

---
+++ Provenance UITests/TabBarControllerUITests.swift
@@ -0,0 +1,33 @@
+import XCTest
+
+class TabBarControllerUITests: XCTestCase {
+  override func setUp() {
+    super.setUp()
+    let app = XCUIApplication()
+    app.launch()
+  }
+
+  func testAccountsTabBarItem() {
+    testTabBarItem(title: "Accounts")
+  }
+
+  func testTagsTabBarItem() {
+    testTabBarItem(title: "Tags")
+  }
+
+  func testCategoriesTabBarItem() {
+    testTabBarItem(title: "Categories")
+  }
+
+  func testAboutTabBarItem() {
+    testTabBarItem(title: "About")
+  }
+
+  func testTabBarItem(title: String) {
+    let elem = XCUIApplication().tabBars["Tab Bar"].buttons[title]
+    XCTAssertTrue(elem.exists)
+    elem.tap()
+    sleep(2)
+    XCTAssertTrue(XCUIApplication().navigationBars[title].exists || XCUIApplication().navigationBars["Error"].exists)
+  }
+}
